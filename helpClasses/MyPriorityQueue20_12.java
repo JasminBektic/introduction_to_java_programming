33457ccd1e85d883267036e0132a915e5037898a
Jasmin Bektic <jaskio89@gmail.com>
2016-10-06 12:32:00 +0200

    Add files via upload

---
+++ helpClasses/MyPriorityQueue20_12.java
@@ -0,0 +1,23 @@
+package helpClasses;
+
+import java.util.PriorityQueue;
+
+@SuppressWarnings("serial")
+public class MyPriorityQueue20_12 extends PriorityQueue<Object> implements Cloneable {
+
+	/** Override clone method defined in the Object class */
+	@Override
+	public MyPriorityQueue20_12 clone() throws CloneNotSupportedException {
+		return (MyPriorityQueue20_12) super.clone();
+	}
+	
+
+	
+
+	
+
+	
+
+	
+
+}
